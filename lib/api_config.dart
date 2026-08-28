8d5fd13b3c7fa3105d6a812c7b571f69eb2bedcb
Nandamad <terserahakun825@gmail.com>
2026-08-28 15:28:26 +0700

    ubah api_config biar bisa online

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -2,7 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConfig {
   static String get baseUrl {
-    // Abaikan dotenv dulu, langsung tembak IP laptopmu secara hardcode
-    return 'http://192.168.18.79:8000/api'; 
+    return 'https://devabs.yukliat.com/api'; 
   }
 }
